7883605ff11527a1608805e0b7d3de5ddbea6dd1
Arshima Sheikh <arshima034btcse23@igdtuw.ac.in>
2026-04-23 22:20:36 +0530

    added functionality to get product by id, modified date structure and response entity format added

--- src/main/java/com/telusko/EcomProject/controller/ProductController.java
+++ src/main/java/com/telusko/EcomProject/controller/ProductController.java
@@ -3,12 +3,13 @@ package com.telusko.EcomProject.controller;
 import com.telusko.EcomProject.model.Product;
 import com.telusko.EcomProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ProductController {
@@ -22,7 +23,16 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product>getAllProducts(){
-        return productService.getAllProducts();
+    public ResponseEntity<List<Product>>getAllProducts(){
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product prod=productService.getProductById(id);
+        if(prod==null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        return new ResponseEntity<>(prod,HttpStatus.OK);
     }
 }
