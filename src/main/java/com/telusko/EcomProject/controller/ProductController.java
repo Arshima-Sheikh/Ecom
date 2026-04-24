a2edecd094295bd9063520e9a5b9ba828edbd4f2
Arshima Sheikh <arshima034btcse23@igdtuw.ac.in>
2026-04-24 17:48:04 +0530

    added add product and image functionality in product

--- src/main/java/com/telusko/EcomProject/controller/ProductController.java
+++ src/main/java/com/telusko/EcomProject/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.telusko.EcomProject.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.telusko.EcomProject.model.Product;
 import com.telusko.EcomProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -34,5 +36,21 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
         return new ResponseEntity<>(prod,HttpStatus.OK);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<?> addProduct(
+            @RequestPart("product") String productJson,
+            @RequestPart("image") MultipartFile imgFile
+    ) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Product p = mapper.readValue(productJson, Product.class);
+
+            Product prod = productService.addProduct(p, imgFile);
+            return new ResponseEntity<>(prod, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
